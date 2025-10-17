eb519aad8d80bbe6c281742b2c4c6c548d342b99
your name <dathaui1@gmail.com>
2025-10-17 09:57:56 +0700

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T>:MonoBehaviour where T: MonoBehaviour
+{
+    public static T instance => _instance;
+    private static T _instance;
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this.GetComponent<T>();
+            return;
+        }
+        if (_instance.gameObject.GetInstanceID() != this.gameObject.GetInstanceID())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
